2648695ed35082d626a323dd450d414175f39409
Freewind <nowindlee@gmail.com>
2023-03-10 17:22:47 +0800

    typescript-vite-tippyjs-getReferenceClientRect-demo

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
-import './index.css';
 import tippy from 'tippy.js';
 import 'tippy.js/dist/tippy.css';
+import './index.css';
 
 const button = document.querySelector("#main button");
 tippy(button!, {
-    content: 'Hello, tippy'
+    content: 'Hello, tippy',
+    placement: 'bottom',
+    getReferenceClientRect: () => {
+        return {
+            width: 100,
+            height: 100,
+            top: 100,
+            left: 0,
+        } as any
+    }
 })
